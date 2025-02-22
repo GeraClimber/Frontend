c77e270a377f441db54517c414398ce64b0d0c3e
gennadiy.glushenkov <gennadi.glushenkov@snowqueen.ru>
2025-02-22 15:21:33 +0700

    add main login navbar components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { MainComponent } from './Components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+];
