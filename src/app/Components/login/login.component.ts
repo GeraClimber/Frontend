ecaba812302aa7bd4565aa20ff98634d11bc7415
gennadiy.glushenkov <gennadi.glushenkov@snowqueen.ru>
2025-02-23 19:00:46 +0700

    feat: Add Login page markup

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -10,6 +10,7 @@ import { MatInputModule } from '@angular/material/input';
 import { Router, RouterLink } from '@angular/router';
 //import { AuthService } from '../../services/auth.service';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
@@ -19,10 +20,43 @@ import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
     MatSnackBarModule,
     MatIconModule,
     ReactiveFormsModule,
+    MatButtonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  //authService = inject(AuthService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
+  hide = true;
+  form!: FormGroup;
+  fb = inject(FormBuilder);
+
+  login() {
+    // this.authService.login(this.form.value).subscribe({
+    //   next: (response) => {
+    //     this.matSnackBar.open(response.message, 'Close', {
+    //       duration: 5000,
+    //       horizontalPosition: 'center',
+    //     });
+    //     this.router.navigate(['/']);
+    //   },
+    //   error: (error) => {
+    //     this.matSnackBar.open(error.error.message, 'Close', {
+    //       duration: 5000,
+    //       horizontalPosition: 'center',
+    //     });
+    //   },
+    // });
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
 }
